a47052efe6808c50b8ea6c693c6cb0bcc9086bec
kguion1 <kguion1@github.com>
2021-02-13 21:20:41 -0800

    adding R_code and data folders

---
+++ R_code/tcga_biolinks.R
@@ -0,0 +1,27 @@
+# if (!requireNamespace("BiocManager", quietly = TRUE)) install.packages("BiocManager")
+#
+if (!require(TCGAbiolinks)) BiocManager::install("TCGAbiolinks")
+
+# if (!requireNamespace("BiocManager", quietly = TRUE)) install.packages("BiocManager")
+# BiocManager::install(c("devtools"))
+# BiocManager::install(c("robustbase"))
+# library(devtools)
+# library(robustbase)
+#
+# devtools::install_github("BioinformaticsFMRP/TCGAbiolinks")
+
+
+
+library(TCGAbiolinks)
+
+#need to edit this line of code to the path of your example_ids.txt
+barcodes <- read.csv("TCGA_PublicData/example_ids.txt")
+
+query <- GDCquery(project = "TCGA-BRCA",
+                  data.category = "Transcriptome Profiling",
+                  data.type = "Gene Expression Quantification",
+                  workflow.type = "HTSeq - Counts"
+                  barcode = c(barcodes))
+
+GDCdownload(query)
+data <- GDCprepare(query)
