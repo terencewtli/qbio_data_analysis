7388a349bf1d9c543241663a737d9e040a57b4c7
Terence Li <terence@usc.edu>
2021-04-19 22:16:04 -0700

    adjusted limma_skeleton

--- R_code/limma_skeleton_code.R
+++ R_code/limma_skeleton_code.R
@@ -8,10 +8,7 @@ library(limma)
 
 # REFERENCE: https://www.bioconductor.org/packages/release/bioc/vignettes/DEqMS/inst/doc/DEqMS-package-vignette.html
 
-clinical <- read.csv("clinical.csv", header = TRUE)
-proteomics <- read.csv("cptac.csv", header = TRUE)
-
-# Read in clinical CSV file to get patient ages
+# Read in clinical CSV file to get patient ages. 
 # clinical <- read.csv("PATH/FILENAME.csv", header = TRUE)
 rownames(clinical) <- clinical$Patient_ID
 
@@ -21,34 +18,42 @@ rownames(clinical) <- clinical$Patient_ID
 clinical$Age.in.Years <- #fill in
 # Use desired cutoff between young and old patients
 clinical$age_category <- #fill in
-
+  
 # We also want to drop missing data using boolean indexing.
 na_mask <- !is.na(clinical$age_category)
 # Use the age_mask and boolean indexing to filter clinical dataset.
 clinical <- # fill in
-
+  
+# Read in proteomics CSV file to get protein abundances.
+# proteomics <- read.csv("PATH/FILENAME.csv", header = TRUE)
 rownames(proteomics) <- proteomics$Patient_ID
 # Drop patient column because we don't want it in transposed data frame
 proteomics$Patient_ID <- NULL
 # Same as clinical, filter proteomics dataset to remove NA patients
 proteomics <- #fill in
-
-# Transpose matrix to desired format - rows = proteins, columns = patients
-proteomics_t <- t(proteomics)
-
+  
+# Use the function t(your_dataframe) to ranspose matrix to desired format.
+# rows = proteins, columns = patients
+proteomics_t <- #fill in
+  
 # Setting up design matrix, which contains categorical variables
 # associated with each patient (young/old in this case)
 f <- factor( clinical$age_category, levels=c("Young", "Old"))
 design <- model.matrix(~0+f, clinical)
 
-# Limma function to fit model
-fit <- lmFit(proteomics_t, design)
+# Use the limma lmFit(data, design_matrix) function
+# and assign the result to a variable called fit
+fit <- #fill in here
 # Contrast matrix to express the different conditions
 # for the linear model. Here we only have young/old
 cont.matrix <- makeContrasts(Y_O="fYoung-fOld", levels=design)
 
-# Use contrasts to fit data. eBayes smooths the error
-fit2 <- contrasts.fit(fit, cont.matrix)
+# Use contrasts to fit data.
+# Save the result of the contrasts.fit(fit, contrasts) 
+# function into fit2
+fit2 <- #fill in here
+  
+# eBayes smooths the error.
 fit3 <- eBayes( fit2 )
 
 # topTable gives statistics for top differentially expressed
